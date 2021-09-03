ecb87b662363d26abbfbde09698e6f030079d8a6
Mathieu Rabine <mrabine@sierrawireless.com>
2021-09-03 10:34:32 +0200

    header only sax code

--- join/sax/include/join/sax.hpp
+++ join/sax/include/join/sax.hpp
@@ -70,35 +70,63 @@ namespace sax
          * @brief get SAX API generic error category name.
          * @return deserializer generic error category name.
          */
-        virtual const char* name () const noexcept;
+        virtual const char* name () const noexcept
+        {
+            return "join";
+        }
 
         /**
          * @brief translate SAX API generic error code to human readable error string.
          * @param code error code.
          * @return human readable error string.
          */
-        virtual std::string message (int code) const;
+        virtual std::string message (int code) const
+        {
+            switch (static_cast <SaxErrc> (code))
+            {
+                case SaxErrc::StackOverflow:
+                    return "stack overflow";
+                case SaxErrc::InvalidParent:
+                    return "parent not an array nor an object";
+                case SaxErrc::InvalidValue:
+                    return "value is invalid";
+                case SaxErrc::ExtraData:
+                    return "extra data detected";
+                default:
+                    return "success";
+            }
+        }
     };
 
     /**
      * @brief get error category.
      * @return the created std::error_category object.
      */
-    const std::error_category& saxCategory ();
+    const std::error_category& saxCategory ()
+    {
+        static SaxCategory instance;
+        return instance;
+    }
 
     /**
      * @brief create an std::error_code object.
      * @param code error code number.
      * @return the created std::error_code object.
      */
-    std::error_code make_error_code (SaxErrc code);
+    std::error_code make_error_code (SaxErrc code)
+    {
+        return std::error_code (static_cast <int> (code), saxCategory ());
+    }
 
     /**
      * @brief create an std::error_condition object.
      * @param code error code number.
      * @return the created std::error_condition object.
      */
-    std::error_condition make_error_condition (SaxErrc code);
+    std::error_condition make_error_condition (SaxErrc code)
+    {
+        return std::error_condition (static_cast <int> (code), saxCategory ());
+    }
 
     /**
      * @brief SAX API interface class.
@@ -241,7 +269,11 @@ namespace sax
          * @brief create instance.
          * @param document document to create.
          */
-        StreamWriter (std::ostream& document);
+        StreamWriter (std::ostream& document)
+        : SaxHandler (),
+          _outstream (document)
+        {
+        }
 
         /**
          * @brief Create the Writer instance by copy.
@@ -277,7 +309,69 @@ namespace sax
          * @param value Value object to serialize.
          * @return 0 on success, -1 otherwise.
          */
-        int serialize (const Value& value);
+        int serialize (const Value& value)
+        {
+            switch (value.index ())
+            {
+                case Value::Null:
+                    return setNull ();
+
+                case Value::Boolean:
+                    return setBool (value.get <Value::Boolean> ());
+
+                case Value::Integer:
+                    return setInt (value.get <Value::Integer> ());
+
+                case Value::Unsigned:
+                    return setUint (value.get <Value::Unsigned> ());
+
+                case Value::Integer64:
+                    return setInt64 (value.get <Value::Integer64> ());
+
+                case Value::Unsigned64:
+                    return setUint64 (value.get <Value::Unsigned64> ());
+
+                case Value::Real:
+                    return setDouble (value.get <Value::Real> ());
+
+                case Value::String:
+                    return setString (value.get <Value::String> ());
+
+                case Value::ArrayValue:
+                    if (startArray (value.size ()) == -1)
+                    {
+                        return -1;
+                    }
+                    for (auto const& element : value.getArray ())
+                    {
+                        if (serialize (element) == -1)
+                        {
+                            return -1;
+                        }
+                    }
+                    return stopArray ();
+
+                case Value::ObjectValue:
+                    if (startObject (value.size ()) == -1)
+                    {
+                        return -1;
+                    }
+                    for (auto const& element : value.getObject ())
+                    {
+                        if ((setKey (element.first) == -1) || (serialize (element.second) == -1))
+                        {
+                            return -1;
+                        }
+                    }
+                    return stopObject ();
+
+                default:
+                    join::lastError = make_error_code (SaxErrc::InvalidValue);
+                    break;
+            }
+
+            return -1;
+        }
 
     protected:
         /**
@@ -285,13 +379,19 @@ namespace sax
          * @param data data to append to output stream.
          * @param size data size.
          */
-        void append (const char* data, uint32_t size);
+        void append (const char* data, uint32_t size)
+        {
+            _outstream.write (data, size);
+        }
 
         /**
          * @brief append data to stream and update data size.
          * @param data data to append to stream.
          */
-        void append (char data);
+        void append (char data)
+        {
+            _outstream.put (data);
+        }
 
         /// output stream.
         std::ostream& _outstream;
@@ -307,7 +407,11 @@ namespace sax
          * @brief default constructor.
          * @param root Value to write.
          */
-        StreamReader (Value& root);
+        StreamReader (Value& root)
+        : SaxHandler (),
+          _root (root)
+        {
+        }
 
         /**
          * @brief copy constructor.
@@ -382,96 +486,242 @@ namespace sax
          * @brief set null value.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setNull () override;
+        virtual int setNull () override
+        {
+            return setValue (Value (in_place_index_t <Value::Null> {}, nullptr));
+        }
 
         /**
          * @brief set boolean value.
          * @param value boolean value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setBool (bool value) override;
+        virtual int setBool (bool value) override
+        {
+            return setValue (Value (in_place_index_t <Value::Boolean> {}, value));
+        }
 
         /**
          * @brief set integer value.
          * @param value integer value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setInt (int32_t value) override;
+        virtual int setInt (int32_t value) override
+        {
+            return setValue (Value (in_place_index_t <Value::Integer> {}, value));
+        }
 
         /**
          * @brief set unsigned integer value.
          * @param value unsigned integer value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setUint (uint32_t value) override;
+        virtual int setUint (uint32_t value) override
+        {
+            return setValue (Value (in_place_index_t <Value::Unsigned> {}, value));
+        }
 
         /**
          * @brief set 64 bits integer value.
          * @param value 64 bits integer value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setInt64 (int64_t value) override;
+        virtual int setInt64 (int64_t value) override
+        {
+            return setValue (Value (in_place_index_t <Value::Integer64> {}, value));
+        }
 
         /**
          * @brief set unsigned 64 bits integer value.
          * @param value unsigned 64 bits integer value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setUint64 (uint64_t value) override;
+        virtual int setUint64 (uint64_t value) override
+        {
+            return setValue (Value (in_place_index_t <Value::Unsigned64> {}, value));
+        }
 
         /**
          * @brief set real value.
          * @param value real value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setDouble (double value) override;
+        virtual int setDouble (double value) override
+        {
+            return setValue (Value (in_place_index_t <Value::Real> {}, value));
+        }
 
         /**
          * @brief set string value.
          * @param value string value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setString (const std::string& value) override;
+        virtual int setString (const std::string& value) override
+        {
+            return setValue (Value (in_place_index_t <Value::String> {}, value));
+        }
 
         /**
          * @brief start array.
          * @param size array size.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int startArray (uint32_t size = 0) override;
+        virtual int startArray (uint32_t size = 0) override
+        {
+            Array array;
+            // reserve at least 2.
+            array.reserve ((size) ? size : 2);
+
+            if (_stack.empty ())
+            {
+                _root = std::move (array);
+                _stack.push (&_root);
+            }
+            else
+            {
+                if (_stack.size () >= _maxdepth)
+                {
+                    join::lastError = make_error_code (SaxErrc::StackOverflow);
+                    return -1;
+                }
+
+                Value::Ptr parent = _stack.top ();
+
+                if (parent->is <Value::ObjectValue> ())
+                {
+                    _stack.push (&parent->insert (Member (_curkey, std::move (array))));
+                    _curkey.clear ();
+                }
+                else if (parent->is <Value::ArrayValue> ())
+                {
+                    _stack.push (&parent->pushBack (std::move (array)));
+                }
+                else
+                {
+                    join::lastError = make_error_code (SaxErrc::InvalidParent);
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
 
         /**
          * @brief stop array.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int stopArray () override;
+        virtual int stopArray () override
+        {
+            if (_stack.size ())
+            {
+                _stack.pop ();
+            }
+
+            return 0;
+        }
 
         /**
          * @brief start object.
          * @param size array size.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int startObject (uint32_t size = 0) override;
+        virtual int startObject (uint32_t size = 0) override
+        {
+            Object object;
+            // reserve at least 2.
+            object.reserve ((size) ? size : 2);
+
+            if (_stack.empty ())
+            {
+                _root = std::move (object);
+                _stack.push (&_root);
+            }
+            else
+            {
+                if (_stack.size () >= _maxdepth)
+                {
+                    join::lastError = make_error_code (SaxErrc::StackOverflow);
+                    return -1;
+                }
+
+                Value::Ptr parent = _stack.top ();
+
+                if (parent->is <Value::ObjectValue> ())
+                {
+                    _stack.push (&parent->insert (Member (_curkey, std::move (object))));
+                    _curkey.clear ();
+                }
+                else if (parent->is <Value::ArrayValue> ())
+                {
+                    _stack.push (&parent->pushBack (std::move (object)));
+                }
+                else
+                {
+                    join::lastError = make_error_code (SaxErrc::InvalidParent);
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
 
         /**
          * @brief set key.
          * @param key object key.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setKey (const std::string& key) override;
+        virtual int setKey (const std::string& key) override
+        {
+            _curkey = key;
+            return 0;
+        }
 
         /**
          * @brief stop object.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int stopObject () override;
+        virtual int stopObject () override
+        {
+            if (_stack.size ())
+            {
+                _stack.pop ();
+            }
+
+            return 0;
+        }
 
         /**
          * @brief set value.
          * @param value value to set.
          * @return 0 on success, -1 otherwise.
          */
-        virtual int setValue (Value&& value);
+        virtual int setValue (Value&& value)
+        {
+            if (_stack.empty ())
+            {
+                join::lastError = make_error_code (SaxErrc::InvalidParent);
+                return -1;
+            }
+
+            Value::Ptr parent = _stack.top ();
+
+            if (parent->is <Value::ObjectValue> ())
+            {
+                parent->insert (Member (_curkey, std::move (value)));
+                _curkey.clear ();
+            }
+            else if (parent->is <Value::ArrayValue> ())
+            {
+                parent->pushBack (std::move (value));
+            }
+            else
+            {
+                join::lastError = make_error_code (SaxErrc::InvalidParent);
+                return -1;
+            }
+
+            return 0;
+        }
 
         /// max stack depth.
         static constexpr size_t _maxdepth = 19;
