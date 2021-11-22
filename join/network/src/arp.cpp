b101c9b6f1fc0d047eb6f576b3b4c136544f16ab
Mathieu Rabine <mrabine@sierrawireless.com>
2021-11-22 11:15:54 +0100

    add arp client

--- join/network/src/arp.cpp
+++ join/network/src/arp.cpp
@@ -159,13 +159,7 @@ MacAddress Arp::request (const IpAddress& ip)
         }
 
         int size = stream.read (reinterpret_cast <char *> (buffer.get ()), stream.canRead ());
-        if (size == -1)
-        {
-            lastError = std::make_error_code (static_cast <std::errc> (errno));
-            break;
-        }
-
-        if (size_t (size) != sizeof (Packet))
+        if (size_t (size) < sizeof (Packet))
         {
             elapsed += std::chrono::duration_cast <std::chrono::milliseconds> (std::chrono::high_resolution_clock::now () - beg);
             continue;
