18d1966f2966b104aa072a3e62e20e73e2dc51a1
mrabine-swi <mrabine@sierrawireless.com>
2023-02-13 15:46:14 +0100

    improve coverage

--- join/core/tests/cache_test.cpp
+++ join/core/tests/cache_test.cpp
@@ -34,6 +34,7 @@
 
 // C.
 #include <unistd.h>
+#include <fcntl.h>
 
 using join::Cache;
 
@@ -42,38 +43,37 @@ using join::Cache;
  */
 class CacheTest : public ::testing::Test
 {
-public:
-    /**
-     * @brief Set up test case.
-     */
-    static void SetUpTestCase ()
-    {
-        std::ofstream outFile;
-
-        outFile.open (file);
-        outFile << fileContent << std::endl;
-        outFile.close ();
-
-        outFile.open (otherFile);
-        outFile << otherFileContent << std::endl;
-        outFile.close ();
-    }
-
-    /**
-     * @brief Tear down test case.
-     */
-    static void TearDownTestCase ()
-    {
-        remove (file.c_str ());
-        remove (otherFile.c_str ());
-    }
-
 protected:
+    /**
+     * @brief Create file.
+     * @param filepath path.
+     * @param content content to write.
+     * @return true on success, false otherwise.
+     */
+    bool writeFile (const std::string& filepath, const std::string& content)
+    {
+        int dst = ::open (filepath.c_str (), O_CREAT | O_TRUNC | O_WRONLY);
+        if (dst == -1)
+        {
+            return false;
+        }
+        if (::write (dst, content.c_str (), content.size ()) != ssize_t (content.size ()))
+        {
+            ::close (dst);
+            return false;
+        }
+        ::fsync (dst);
+        ::close (dst);
+        return true;
+    }
+
     /**
      * @brief Set up test.
      */
     virtual void SetUp ()
     {
+        ASSERT_TRUE (writeFile (file, fileContent)) << strerror (errno);
+        ASSERT_TRUE (writeFile (otherFile, otherFileContent)) << strerror (errno);
         ASSERT_NE (nullptr, cache.get (file));
         ASSERT_NE (nullptr, cache.get (otherFile));
     }
@@ -84,6 +84,8 @@ protected:
     virtual void TearDown ()
     {
         ASSERT_NO_THROW (cache.clear ());
+        remove (otherFile.c_str ());
+        remove (file.c_str ());
     }
 
     /// server instance.
@@ -100,6 +102,9 @@ protected:
 
     /// other file content.
     static const std::string otherFileContent;
+
+    /// bad file name.
+    static const std::string bad;
 };
 
 Cache             CacheTest::cache;
@@ -107,23 +112,34 @@ const std::string CacheTest::file             = "/tmp/cache_test";
 const std::string CacheTest::fileContent      = "test string";
 const std::string CacheTest::otherFile        = "/tmp/cache_other_test";
 const std::string CacheTest::otherFileContent = "other test string";
+const std::string CacheTest::bad              = "/tmp/cache_bad_test";
 
 /**
  * @brief Test get method.
  */
 TEST_F (CacheTest, get)
 {
+    ASSERT_EQ (cache.get (bad), nullptr);
+
     struct stat sbuf;
+    ASSERT_EQ (stat (file.c_str (), &sbuf), 0) << strerror (errno);
+    ASSERT_EQ (cache.get (bad, &sbuf), nullptr);
 
-    void* fi = cache.get (file);
-    ASSERT_NE (fi, nullptr);
-    ASSERT_EQ (stat (file.c_str (), &sbuf), 0);
-    ASSERT_STREQ (std::string (reinterpret_cast <char const*> (fi), sbuf.st_size - 1).c_str (), fileContent.c_str ());
+    ASSERT_EQ (stat (file.c_str (), &sbuf), 0) << strerror (errno);
+    char* data = reinterpret_cast <char*> (cache.get (file, &sbuf));
+    ASSERT_NE (data, nullptr);
+    ASSERT_EQ (std::string (data, sbuf.st_size), fileContent);
 
-    fi = cache.get (otherFile);
-    ASSERT_NE (fi, nullptr);
-    ASSERT_EQ (stat (otherFile.c_str (), &sbuf), 0);
-    ASSERT_STREQ (std::string (reinterpret_cast <char const*> (fi), sbuf.st_size - 1).c_str (), otherFileContent.c_str ());
+    ASSERT_EQ (stat (otherFile.c_str (), &sbuf), 0) << strerror (errno);
+    data = reinterpret_cast <char*> (cache.get (otherFile));
+    ASSERT_NE (data, nullptr);
+    ASSERT_EQ (std::string (data, sbuf.st_size), otherFileContent);
+
+    ASSERT_TRUE (writeFile (file, otherFileContent)) << strerror (errno);
+    ASSERT_EQ (stat (file.c_str (), &sbuf), 0) << strerror (errno);
+    data = reinterpret_cast <char*> (cache.get (file));
+    ASSERT_NE (data, nullptr);
+    ASSERT_EQ (std::string (data, sbuf.st_size), otherFileContent);
 }
 
 /**
