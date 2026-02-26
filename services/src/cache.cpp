5621962ca9f71c3594cb7b4cc5cb02fa2a5cbef2
mrabine <mathieu.rabine@vector.com>
2026-02-26 19:50:02 +0100

    add webbench sample

--- services/src/cache.cpp
+++ services/src/cache.cpp
@@ -49,19 +49,19 @@ Cache::~Cache ()
 //   CLASS     : Cache
 //   METHOD    : get
 // =========================================================================
-void* Cache::get (const std::string &fileName, struct stat &sbuf)
+void* Cache::get (const std::string& fileName, struct stat& sbuf)
 {
     if ((stat (fileName.c_str (), &sbuf) < 0) || S_ISDIR (sbuf.st_mode))
     {
         return nullptr;
     }
 
-    ScopedLock <Mutex> lock (_mutex);
+    ScopedLock<Mutex> lock (_mutex);
 
     auto it = _entries.find (fileName);
     if (it != _entries.end ())
     {
-        if (it->second->modifTime.tv_sec  == sbuf.st_ctim.tv_sec &&
+        if (it->second->modifTime.tv_sec == sbuf.st_ctim.tv_sec &&
             it->second->modifTime.tv_nsec == sbuf.st_ctim.tv_nsec)
         {
             return it->second->addr;
@@ -76,12 +76,12 @@ void* Cache::get (const std::string &fileName, struct stat &sbuf)
     int fd = open (fileName.c_str (), O_RDONLY);
     if (fd >= 0)
     {
-        std::unique_ptr <CacheEntry> entry = std::make_unique <CacheEntry> ();
+        std::unique_ptr<CacheEntry> entry = std::make_unique<CacheEntry> ();
         if (entry != nullptr)
         {
-            entry->size = sbuf.st_size;
+            entry->size      = sbuf.st_size;
             entry->modifTime = sbuf.st_ctim;
-            entry->addr = mmap (0, entry->size, PROT_READ, MAP_PRIVATE, fd, 0);
+            entry->addr      = mmap (0, entry->size, PROT_READ, MAP_PRIVATE, fd, 0);
             if (entry->addr != MAP_FAILED)
             {
                 ptr = _entries.emplace (fileName, std::move (entry)).first->second->addr;
@@ -98,9 +98,9 @@ void* Cache::get (const std::string &fileName, struct stat &sbuf)
 //   CLASS     : Cache
 //   METHOD    : remove
 // =========================================================================
-void Cache::remove (const std::string &fileName)
+void Cache::remove (const std::string& fileName)
 {
-    ScopedLock <Mutex> lock (_mutex);
+    ScopedLock<Mutex> lock (_mutex);
 
     auto it = _entries.find (fileName);
     if (it != _entries.end ())
@@ -116,7 +116,7 @@ void Cache::remove (const std::string &fileName)
 // =========================================================================
 void Cache::clear ()
 {
-    ScopedLock <Mutex> lock (_mutex);
+    ScopedLock<Mutex> lock (_mutex);
 
     for (auto const& entry : _entries)
     {
@@ -132,7 +132,7 @@ void Cache::clear ()
 // =========================================================================
 size_t Cache::size ()
 {
-    ScopedLock <Mutex> lock (_mutex);
+    ScopedLock<Mutex> lock (_mutex);
 
     return _entries.size ();
 }
